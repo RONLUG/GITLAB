604a6c8c97a381acc28d4c4ef42e0a2cac32d4f8
ClassySeal <ron.lugsami@gmail.com>
2023-09-24 14:24:59 +0700

    Add new changes

--- bloodgroup.c
+++ bloodgroup.c
@@ -4,11 +4,13 @@
 // A still here!
 // Hello A!
 // B was here too!
+// Branching
 void main()
 { char Name[40], G[5], answer[10];
   int i;
   do
   {
+    
     printf("What is your Name? ");
     scanf("%s", Name);
     do
